0b19a71a56d7b0261b307edb67bdc6abd50cf093
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-02-06 20:28:14 +0300

    update frontend code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: '.', 
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html', 
-    },
-  },
-});
+  plugins: [react()],
+})
