896722c9eea189667af68bfd0c8b4b2ad4da47ac
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-02-12 18:13:14 +0300

    update header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 import lightLogo from "../header/logoD.png";
 import darkLogo from "../header/logo.png";
 
 const Header = ({ toggleTheme, theme }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = (e) => {
+    if (!e.target.closest(".nav-container") && menuOpen) {
+      setMenuOpen(false);
+    }
+  };
+
   return (
-    <header className="header">
+    <header className="header" onClick={closeMenu}>
       <div className="logo-container">
         <img
           src={theme === "light" ? lightLogo : darkLogo}
@@ -14,20 +26,33 @@ const Header = ({ toggleTheme, theme }) => {
           className="logo"
         />
       </div>
-      <div className="nav-container">
+
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <div className={`nav-container ${menuOpen ? "active" : ""}`}>
         <nav>
-          <ul className="nav-list">
+          <ul className={`nav-list ${menuOpen ? "active" : ""}`}>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={() => setMenuOpen(false)}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/user-form">Register</Link>
+              <Link to="/user-form" onClick={() => setMenuOpen(false)}>
+                Register
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={() => setMenuOpen(false)}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={() => setMenuOpen(false)}>
+                Contact
+              </Link>
             </li>
             <li>
               <button className="theme-toggle" onClick={toggleTheme}>
